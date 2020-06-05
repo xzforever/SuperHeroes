2d4135505e2fd96663cbb2c1e0502bab2228c409
xzforever <xzforever.1234@gmail.com>
2020-06-06 00:43:50 +0300

    Добавьте файлы проекта.

---
+++ 2article1_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2article1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperMan superman = new SuperMan();
+            IronMan ironman = new IronMan();
+            SuperWoman superwoman = new SuperWoman();
+            Thor thor = new Thor();
+            AmericaCaptain americacaptain = new AmericaCaptain();
+            BatMan batman = new BatMan();
+            Vision vision = new Vision();
+        }
+    }
+}
